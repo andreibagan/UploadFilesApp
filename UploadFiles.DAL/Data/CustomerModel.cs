7b7c21d3b6267235e818f18a43cb959b1539e4fe
andreibagan <a.bagan@beesender.com>
2023-10-01 15:42:34 +0300

    Associating File Uploads with a Form

---
+++ UploadFiles.DAL/Data/CustomerModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace UploadFiles.DAL.Data
+{
+    public class CustomerModel
+    {
+        public Guid Id { get; set; }
+        public string UserName { get; set; } = "bagan";
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FileName { get; set; }
+        public long Size { get; set; }
+    }
+}
